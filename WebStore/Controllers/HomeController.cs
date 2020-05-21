469ea030a33d88f91dcfe92fb3dfa94170dfd072
Infarh <shmachilin@yandex.ru>
2020-05-21 20:40:33 +0300

    Главный контроллер Home и первые два действия

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("First controller action result!");
+        }
+
+        public IActionResult AnotherAction()
+        {
+            return Content("Another action result");
+        }
+    }
+}
