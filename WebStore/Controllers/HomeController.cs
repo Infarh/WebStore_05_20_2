67518d0f4b538acb7449e4ded7a46a12e1c4e412
Infarh <shmachilin@yandex.ru>
2020-05-21 20:59:32 +0300

    Передача данных (модели) в представление

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Surname = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 50
+            },
+            new Employee
+            {
+                Id = 2,
+                Surname = "Петров",
+                FirstName = "Пётр",
+                Patronymic = "Петрович",
+                Age = 25
+            },
+            new Employee
+            {
+                Id = 3,
+                Surname = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 30
+            },
+        };
+
+
         public IActionResult Index()
         {
-            return View();
+            //ViewBag.Title = "Hello World";
+            ViewData["Title"] = "Hello World!";
+
+            return View(__Employees);
         }
 
         public IActionResult AnotherAction()
