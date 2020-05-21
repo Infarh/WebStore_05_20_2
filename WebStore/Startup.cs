11d0d08142421e407165e43ddb1f5975d4b6a9a6
Infarh <shmachilin@yandex.ru>
2020-05-21 20:21:06 +0300

    Использование конфигурации при настройки приложения

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,13 @@ namespace WebStore
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -21,11 +29,13 @@ namespace WebStore
 
             app.UseRouting();
 
+            var config_string = Configuration["CustomGreetings"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(config_string);
                 });
             });
         }
