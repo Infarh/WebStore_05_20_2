cee997b5296bc9b0a9dbb308eb07b3fed0cf3b2f
Infarh <shmachilin@yandex.ru>
2020-05-25 21:10:53 +0300

    Добавление заготовки действий для основных представлений в контроллер Home

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,5 +10,21 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        public IActionResult Blog() => View();
+        
+        public IActionResult BlogSingle() => View();
+        
+        public IActionResult Cart() => View();
+        
+        public IActionResult CheckOut() => View();
+        
+        public IActionResult ContactUs() => View();
+        
+        public IActionResult Login() => View();
+        
+        public IActionResult ProductDetails() => View();
+        
+        public IActionResult Shop() => View();
     }
 }
