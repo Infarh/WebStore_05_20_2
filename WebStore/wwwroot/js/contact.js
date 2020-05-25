4a174ea1ca67d02c4c41ec505519fd9e07341162
Infarh <shmachilin@yandex.ru>
2020-05-25 19:59:54 +0300

    Исправление ошибок в стилях и скриптах шаблона

--- WebStore/wwwroot/js/contact.js
+++ WebStore/wwwroot/js/contact.js
@@ -1,7 +1,10 @@
-jQuery(function($) {'use strict',
+jQuery(function($) {
+    'use strict';
 
 	var form = $('.contact-form');
-	form.submit(function () {'use strict',
+	form.submit(function () {
+		'use strict';
+
 		$this = $(this);
 		$.post("sendemail.php", $(".contact-form").serialize(),function(result){
 			if(result.type == 'success'){
