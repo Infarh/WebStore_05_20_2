9798a186e7d85543d2bc832213f780c2356a6308
Infarh <shmachilin@yandex.ru>
2020-06-01 20:39:45 +0300

    Базовые классы сущностей

---
+++ WebStore.Domain/Entityes/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entityes/Base/NamedEntity.cs
@@ -0,0 +1,9 @@
+﻿using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public abstract class NamedEntity : BaseEntity, INamedEntity
+    {
+        public string Name { get; set; }
+    }
+}
