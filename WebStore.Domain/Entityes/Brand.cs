cc8e20309b9d1dfc5a07a7567acfb6eb7a40a166
Infarh <shmachilin@yandex.ru>
2020-06-01 20:42:27 +0300

    Определение Секции и Бренда

---
+++ WebStore.Domain/Entityes/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entityes/Section.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
