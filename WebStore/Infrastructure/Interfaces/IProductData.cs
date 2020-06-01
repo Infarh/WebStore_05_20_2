698232313f92c65ab5951e492c35ec44b701e993
Infarh <shmachilin@yandex.ru>
2020-06-01 20:47:55 +0300

    Сервис каталога товаров в памяти

---
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+    }
+}
---
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+    }
+}
