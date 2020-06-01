d0c928c2ac3d092d04abbfc6fbb51a905dbe79fb
Infarh <shmachilin@yandex.ru>
2020-06-01 21:36:16 +0300

    Добавление контроллера каталога товаров

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Shop(int? SectionId, int? BrandId)
+        {
+            var filter = new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).OrderBy(p => p.Order)
+            });
+        }
+    }
+}
