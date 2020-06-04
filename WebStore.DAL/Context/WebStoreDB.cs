e0a13568e9832b2887d335fd78936b92957084d9
Infarh <shmachilin@yandex.ru>
2020-06-04 20:24:38 +0300

    Определение контекста БД

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
 
 namespace WebStore.DAL.Context
 {
-    public class WebStoreDB
+    public class WebStoreDB : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+        
+        public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
     }
 }
