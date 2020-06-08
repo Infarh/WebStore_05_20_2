98a21730549d7a5e4add58dbcf6fc1c62988ebf5
Infarh <shmachilin@yandex.ru>
2020-06-08 20:34:04 +0300

    Заготовка контроллера Account - основные действия

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => View();
+
+        public IActionResult AccessDenied() => View();
+    }
+}
