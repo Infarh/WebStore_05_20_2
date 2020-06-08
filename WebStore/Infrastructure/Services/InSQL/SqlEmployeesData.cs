85f81e449369f958863a32153bd89607a04f31f5
Infarh <shmachilin@yandex.ru>
2020-06-08 19:56:54 +0300

    Логика сервиса сотрудников для хранения данных в памяти

--- WebStore/Infrastructure/Services/InSQL/SqlEmployeesData.cs
+++ WebStore/Infrastructure/Services/InSQL/SqlEmployeesData.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Employees;
 using WebStore.Infrastructure.Interfaces;
@@ -11,16 +13,52 @@ namespace WebStore.Infrastructure.Services.InSQL
 
         public SqlEmployeesData(WebStoreDB db) => _db = db;
 
-        public IEnumerable<Employee> Get() { throw new System.NotImplementedException(); }
+        public IEnumerable<Employee> Get() => _db.Employees/*.AsEnumerable()*/;
 
-        public Employee GetById(int id) { throw new System.NotImplementedException(); }
+        public Employee GetById(int id) => /*_db.Employees.Find(id);*/ _db.Employees.FirstOrDefault(e => e.Id == id);
 
-        public int Add(Employee Employee) { throw new System.NotImplementedException(); }
+        public int Add(Employee Employee)
+        {
+            if (Employee is null) throw new ArgumentNullException(nameof(Employee));
+            if (Employee.Id != 0) throw new InvalidOperationException("Для добавляемого сотрудника вручную задан первичный ключ");
 
-        public void Edit(Employee Employee) { throw new System.NotImplementedException(); }
+            _db.Employees.Add(Employee /*?? throw new ArgumentNullException(nameof(Employee))*/);
 
-        public bool Delete(int id) { throw new System.NotImplementedException(); }
+            return Employee.Id;
+        }
 
-        public void SaveChanges() { throw new System.NotImplementedException(); }
+        public void Edit(Employee Employee)
+        {
+            if (Employee is null) throw new ArgumentNullException(nameof(Employee));
+
+            //var db_item = GetById(Employee.Id);
+            //if(db_item is null) return;
+
+            //db_item.FirstName = Employee.FirstName;
+            //db_item.Surname = Employee.Surname;
+            //db_item.Patronymic = Employee.Patronymic;
+            //db_item.Age = Employee.Age;
+
+            //_db.Attach(Employee);
+            //_db.Entry(Employee).State = EntityState.Modified;
+
+            _db.Update(Employee);
+        }
+
+        public bool Delete(int id)
+        {
+            var employee = _db.Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null) return false;
+
+            //_db.Employees.Remove(employee);
+
+            //_db.Entry(employee).State = EntityState.Deleted;
+
+            _db.Remove(employee);
+
+            return true;
+        }
+
+        public void SaveChanges() => _db.SaveChanges();
     }
 }
