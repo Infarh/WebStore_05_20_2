597802c30994c5c92f1ab00b92b6f1ce09d44e79
Infarh <shmachilin@yandex.ru>
2020-06-08 20:09:13 +0300

    Модификация контекста БД для того, чтобы Identity смогла хранить в нём свои данные

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Employees;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.DAL.Context
 {
-    public class WebStoreDB : DbContext
+    public class WebStoreDB : IdentityDbContext<User, Role, string>
     {
         public DbSet<Product> Products { get; set; }
 
