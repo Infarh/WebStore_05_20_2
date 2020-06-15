edb01fe84a438e2a0f68f68167aff1a2474bef9d
Infarh <shmachilin@yandex.ru>
2020-06-15 20:06:06 +0300

    Инициализация пользователя Администратор и ролей администратор и пользователь

--- WebStore/Data/WebStoreDBInitializer.cs
+++ WebStore/Data/WebStoreDBInitializer.cs
@@ -1,13 +1,25 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
-        public WebStoreDBInitializer(WebStoreDB db) => _db = db;
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
+
+        public WebStoreDBInitializer(WebStoreDB db, UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        {
+            _db = db;
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
+        }
 
         public void Initialize()
         {
@@ -18,19 +30,36 @@ namespace WebStore.Data
             //        throw new InvalidOperationException("Ошибка при создании базы данных товаров");
 
             db.Migrate();
-            
-            if(!_db.Employees.Any())
-                using (db.BeginTransaction())
-                {
-                    var employees = TestData.Employees.ToList();
-                    //foreach (var employee in employees)
-                    //    employee.Id = 0;
-                    employees.ForEach(e => e.Id = 0);
 
-                    _db.Employees.AddRange(employees);
-                }
+            InitializeEmployees();
 
-            if(_db.Products.Any()) return;
+            InitializeProducts();
+
+            InitializeIdentityAsync().Wait();
+        }
+
+        private void InitializeEmployees()
+        {
+            var db = _db.Database;
+
+            if (_db.Employees.Any()) return;
+
+            using (db.BeginTransaction())
+            {
+                var employees = TestData.Employees.ToList();
+                //foreach (var employee in employees)
+                //    employee.Id = 0;
+                employees.ForEach(e => e.Id = 0);
+
+                _db.Employees.AddRange(employees);
+            }
+        }
+
+        private void InitializeProducts()
+        {
+            var db = _db.Database;
+
+            if (_db.Products.Any()) return;
 
             using (db.BeginTransaction())
             {
@@ -64,6 +93,29 @@ namespace WebStore.Data
                 db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Products] OFF");
 
                 transaction.Commit();
+            }
+        }
+
+        private async Task InitializeIdentityAsync()
+        {
+            if (!await _RoleManager.RoleExistsAsync(Role.Administrator))
+                await _RoleManager.CreateAsync(new Role { Name = Role.Administrator });
+
+            if (!await _RoleManager.RoleExistsAsync(Role.User))
+                await _RoleManager.CreateAsync(new Role { Name = Role.User });
+
+            if (await _UserManager.FindByNameAsync(User.Administrator) is null)
+            {
+                var admin = new User { UserName = User.Administrator };
+
+                var create_result = await _UserManager.CreateAsync(admin, User.DefaultAdminPassword);
+                if (create_result.Succeeded)
+                    await _UserManager.AddToRoleAsync(admin, Role.Administrator);
+                else
+                {
+                    var errors = create_result.Errors.Select(e => e.Description);
+                    throw new InvalidOperationException($"Ошибка при создании пользователя Администратора: {string.Join(",", errors)}");
+                }
             }
         }
     }
