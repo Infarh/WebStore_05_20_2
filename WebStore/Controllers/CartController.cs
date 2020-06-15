7d5247fc4a8cdbd65c9ff923a3e6d891a5c552c5
Infarh <shmachilin@yandex.ru>
2020-06-15 21:33:41 +0300

    Ссылки на корзину в представлениях

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _CartService;
+
+        public CartController(ICartService CartService) => _CartService = CartService;
+
+        public IActionResult Details() => View(_CartService.TransformFromCart());
+
+        public IActionResult AddToCart(int id)
+        {
+            _CartService.AddToCart(id);
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult DecrementFromCart(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return RedirectToAction(nameof(Details));
+        }
+
+        public IActionResult RemoveAll()
+        {
+            _CartService.RemoveAll();
+            return RedirectToAction(nameof(Details));
+        }
+    }
+}
