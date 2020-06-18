e803144121aa8fc002b4b33e6106bf3a836a19a6
Infarh <shmachilin@yandex.ru>
2020-06-18 19:30:41 +0300

    ГРАБЛИ!!! с областями...

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CatalogController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
