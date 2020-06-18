cef8e9602320ceff6cc08e5c61eaf8a9be3dcbc5
Infarh <shmachilin@yandex.ru>
2020-06-18 20:54:17 +0300

    Добавление сущности Заказ в БД + Миграция

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Employees;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
 
 namespace WebStore.DAL.Context
 {
@@ -15,6 +16,10 @@ namespace WebStore.DAL.Context
         public DbSet<Brand> Brands { get; set; }
 
         public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
         
         public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
     }
