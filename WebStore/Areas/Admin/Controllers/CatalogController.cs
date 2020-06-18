3b048cf2d752ff76d6220f034ec5bb9017276308
Infarh <shmachilin@yandex.ru>
2020-06-18 19:50:35 +0300

    Попытка исправить макет области администратора

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CatalogController : Controller
     {
+        private readonly IProductData _ProductData;
 
-        public IActionResult Index()
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index() => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id)
         {
-            return View();
+            var product = id is null ? new Product() : _ProductData.GetProductById((int) id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
         }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
 }
