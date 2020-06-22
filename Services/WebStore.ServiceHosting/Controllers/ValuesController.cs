730bc23bea57dbec8124cd7c74c89eedc2961851
Infarh <shmachilin@yandex.ru>
2020-06-22 20:35:48 +0300

    Контроллер, реализующий работу тестового WebAPI

---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> __Values = Enumerable
+           .Range(1, 10)
+           .Select(i => $"Value-{i}")
+           .ToList();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() => __Values;
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= __Values.Count)
+                return NotFound();
+
+            return __Values[id];
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, string value)
+        {
+            if (id < 0 || id >= __Values.Count)
+                return BadRequest();
+
+            __Values[id] = value;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= __Values.Count)
+                return NotFound();
+
+            __Values.RemoveAt(id);
+
+            return Ok();
+        }
+    }
+}
