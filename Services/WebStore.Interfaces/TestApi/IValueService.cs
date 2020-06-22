65df99b8f5307b0993ca79a2cd8b579e8f6f77e8
Infarh <shmachilin@yandex.ru>
2020-06-22 20:25:18 +0300

    Интерфейс тестового WebAPI

---
+++ Services/WebStore.Interfaces/TestApi/IValueService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace WebStore.Interfaces.TestApi
+{
+    public interface IValueService
+    {
+        IEnumerable<string> Get();
+
+        string Get(int id);
+
+        Uri Post(string value);
+
+        HttpStatusCode Update(int id, string value);
+
+        HttpStatusCode Delete(int id);
+    }
+}
