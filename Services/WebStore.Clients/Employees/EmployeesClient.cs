0314a0baaf3d725b17513ffc02cc1781a19547d4
Infarh <shmachilin@yandex.ru>
2020-06-25 19:53:37 +0300

    Заготовка клиента для WebAPI управления сотрудниками

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Employees;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees) { }
+
+        public IEnumerable<Employee> Get() { throw new System.NotImplementedException(); }
+
+        public Employee GetById(int id) { throw new System.NotImplementedException(); }
+
+        public int Add(Employee Employee) { throw new System.NotImplementedException(); }
+
+        public void Edit(Employee Employee) { throw new System.NotImplementedException(); }
+
+        public bool Delete(int id) { throw new System.NotImplementedException(); }
+
+        public void SaveChanges() { throw new System.NotImplementedException(); }
+    }
+}
