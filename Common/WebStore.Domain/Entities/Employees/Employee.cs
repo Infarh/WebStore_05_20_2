e1a9aca21604ced5a0fd43c5086a03553f8a2cb4
Infarh <shmachilin@yandex.ru>
2020-06-29 21:13:56 +0300

    Конфигурирование Swagger

--- Common/WebStore.Domain/Entities/Employees/Employee.cs
+++ Common/WebStore.Domain/Entities/Employees/Employee.cs
@@ -3,16 +3,21 @@ using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities.Employees
 {
+    /// <summary>Информация о сотруднике</summary>
     public class Employee : BaseEntity
     {
+        /// <summary>Имя</summary>
         [Required]
         public string FirstName { get; set; }
 
+        /// <summary>Фамилия</summary>
         [Required]
         public string Surname { get; set; }
 
+        /// <summary>Отчество</summary>
         public string Patronymic { get; set; }
 
+        /// <summary>Возраст</summary>
         public int Age { get; set; }
     }
 }
