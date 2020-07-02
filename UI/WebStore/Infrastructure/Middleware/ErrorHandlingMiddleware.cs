de1aed2362132a4f8115bcad3ba58df363037675
Infarh <shmachilin@yandex.ru>
2020-07-02 21:15:04 +0300

    ЗАготовка промежуточного ПО обработки ошибок

---
+++ UI/WebStore/Infrastructure/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebStore.Infrastructure.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public ErrorHandlingMiddleware(RequestDelegate Next)
+        {
+            _Next = Next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _Next(context);
+        }
+    }
+}
