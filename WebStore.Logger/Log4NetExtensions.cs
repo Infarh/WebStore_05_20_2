8d50a1e016453c699bddb3656846673f54aa289c
Infarh <shmachilin@yandex.ru>
2020-07-02 20:48:42 +0300

    Система интеграции бибилотеки Log4NET в инфраструктуру логирования ASP.NET Core

---
+++ WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4net.config")
+        {
+            if (!Path.IsPathRooted(ConfigurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не определена сборка с точкой входа в прлиожение");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Не удалось определить путь к дирректории исполнительной сборки");
+                ConfigurationFile = Path.Combine(dir, ConfigurationFile);
+            }
+
+            Factory.AddProvider(new Log4NetLoggerProvider(ConfigurationFile));
+
+            return Factory;
+        }
+    }
+}
