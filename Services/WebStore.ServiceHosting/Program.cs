04ca821eb3de4bf359c93d7c93bb50afa591632e
Infarh <shmachilin@yandex.ru>
2020-07-02 19:50:07 +0300

    Возможности конфигуировния системы логирования

--- Services/WebStore.ServiceHosting/Program.cs
+++ Services/WebStore.ServiceHosting/Program.cs
@@ -1,5 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 
 namespace WebStore.ServiceHosting
 {
@@ -9,6 +14,24 @@ namespace WebStore.ServiceHosting
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.ConfigureLogging((host, log) => log
+                //   .AddConfiguration(host.Configuration)
+                //   .ClearProviders()
+                //   .AddConsole())
+                //.ConfigureLogging((host, log) =>
+                //{
+                //    log.ClearProviders();
+                //    //log.AddProvider(new ConsoleLoggerProvider())
+                //    log.AddConsole(opt => opt.IncludeScopes = true);
+                //    log.AddEventLog(opt => opt.LogName = "WebStore.log");
+                //    log.AddFilter((str, level) =>
+                //    {
+                //        if (str.Contains("test", StringComparison.OrdinalIgnoreCase))
+                //            return level >= LogLevel.Debug;
+                //        return true;
+                //    });
+                //    log.AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Information);
+                //})
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
                 );
--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
