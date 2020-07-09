2b219ed483b9e89b5ec8cd44f2908bfa414f2827
Infarh <shmachilin@yandex.ru>
2020-07-09 20:31:54 +0300

    Заготовка визуального компонента отображения хлебных крошек

---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
