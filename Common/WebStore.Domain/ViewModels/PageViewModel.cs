ea31c82ba72e154d72e3f7bdd34876692135f417
Infarh <shmachilin@yandex.ru>
2020-07-13 20:46:43 +0300

    Модель-представления для информации о текущей странице каталога товаров

---
+++ Common/WebStore.Domain/ViewModels/PageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class PageViewModel
+    {
+        public int TotalItems { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int TotalPages => (int) Math.Ceiling((decimal) TotalItems / PageSize);
+    }
+}
--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
 using WebStore.Interfaces.Services;
@@ -11,15 +12,26 @@ namespace WebStore.Controllers
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
+        private readonly IConfiguration _Configuration;
 
-        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
-
-        public IActionResult Shop(int? SectionId, int? BrandId, [FromServices] IMapper Mapper)
+        public CatalogController(IProductData ProductData, IConfiguration Configuration)
         {
+            _ProductData = ProductData;
+            _Configuration = Configuration;
+        }
+
+        public IActionResult Shop(int? SectionId, int? BrandId, [FromServices] IMapper Mapper, int Page = 1)
+        {
+            var page_size = int.TryParse(_Configuration["PageSize"], out var size)
+                ? size
+                : (int?) null;
+
             var filter = new ProductFilter
             {
                 SectionId = SectionId,
-                BrandId = BrandId
+                BrandId = BrandId,
+                Page = Page,
+                PageSize = page_size
             };
 
             var products = _ProductData.GetProducts(filter);
@@ -32,7 +44,13 @@ namespace WebStore.Controllers
                    .Products
                    .Select(p => p.FromDTO())
                    .Select(Mapper.Map<ProductViewModel>)
-                   .OrderBy(p => p.Order)
+                   .OrderBy(p => p.Order),
+                PageViewModel = new PageViewModel
+                {
+                    PageSize = page_size ?? 0,
+                    PageNumber = Page,
+                    TotalItems = products.TotalCount
+                }
             });
         }
 
