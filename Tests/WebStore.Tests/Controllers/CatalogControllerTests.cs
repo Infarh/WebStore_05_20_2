9ac8eb4cc128adbc3f67fa90a702477ffba01390
Infarh <shmachilin@yandex.ru>
2020-07-13 21:05:08 +0300

    Исправление ошибок в модульных тестах

--- Tests/WebStore.Tests/Controllers/CatalogControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/CatalogControllerTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WebStore.Controllers;
@@ -53,7 +54,10 @@ namespace WebStore.Tests.Controllers
                    }
                });
 
-            var controller = new CatalogController(product_data_mock.Object);
+            var configuration_mock = new Mock<IConfiguration>();
+            configuration_mock.Setup(cfg => cfg["PageSize"]).Returns("3");
+
+            var controller = new CatalogController(product_data_mock.Object, configuration_mock.Object);
 
             #endregion
 
@@ -128,7 +132,10 @@ namespace WebStore.Tests.Controllers
                     TotalCount = products.Length
                 });
 
-            var controller = new CatalogController(product_data_mock.Object);
+            var configuration_mock = new Mock<IConfiguration>();
+            configuration_mock.Setup(cfg => cfg["PageSize"]).Returns("3");
+
+            var controller = new CatalogController(product_data_mock.Object, configuration_mock.Object);
 
             const int expected_section_id = 1;
             const int expected_brand_id = 5;
