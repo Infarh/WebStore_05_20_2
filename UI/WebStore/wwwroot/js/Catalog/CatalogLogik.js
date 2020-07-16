1c7a65d4ab5ef93c02d644fd4d9a859c1f7d0a06
Infarh <shmachilin@yandex.ru>
2020-07-16 19:32:25 +0300

    Скрипт асинхронного переключения страниц каталога товаров

--- UI/WebStore/wwwroot/js/Catalog/CatalogLogik.js
+++ UI/WebStore/wwwroot/js/Catalog/CatalogLogik.js
@@ -13,6 +13,37 @@
 
         const button = $(this);
 
+        if (button.prop("href").length > 0) {
+            const page = button.data("page");
 
+            const container = $("#catalog-items-container");
+
+            container.LoadingOverlay("show");
+
+            const data = button.data();
+
+            let query = "";
+            for (let key in data) {
+                if (data.hasOwnProperty(key))
+                    query += key + "=" + data[key] + "&";
+            }
+
+            $.get(Catalog._properties.getUrl + "?" + query)
+                .done(catalogHtml => {
+                    container.html(catalogHtml);
+                    container.LoadingOverlay("hide");
+
+                    $(".pagination li").removeClass("active");
+                    $(".pagination li a").prop("href", "#");
+
+                    $(`.pagination li a[data-page=${page}]`)
+                        .removeAttr("href")
+                        .parent().addClass("active");
+                })
+                .fail(() => {
+                    container.LoadingOverlay("hide");
+                    console.log("clickOnPage fail");
+                });
+        }
     }
 }
