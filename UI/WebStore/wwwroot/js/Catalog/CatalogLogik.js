b10273a4904690ba6821a1e16fdc24921887038e
Infarh <shmachilin@yandex.ru>
2020-07-16 19:18:09 +0300

    Заготовка скрипта асинхронного обновления страниц каталога товаров

---
+++ UI/WebStore/wwwroot/js/Catalog/CatalogLogik.js
@@ -0,0 +1,18 @@
+﻿Catalog = {
+    _properties: {
+        getUrl: ""
+    },
+
+    init: properties => {
+        $.extend(Catalog._properties, properties);
+        $(".pagination li a").click(Catalog.clickOnPage);
+    },
+
+    clickOnPage: function(event) {
+        event.preventDefault();
+
+        const button = $(this);
+
+
+    }
+}
