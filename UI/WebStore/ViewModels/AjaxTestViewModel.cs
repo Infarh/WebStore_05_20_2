44e9f2b3fe0e382f9981260586ae4671fa4872c7
Infarh <shmachilin@yandex.ru>
2020-07-16 20:21:07 +0300

    Заготовка для теста ненавязчивого ajax

---
+++ UI/WebStore/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebStore.ViewModels
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
